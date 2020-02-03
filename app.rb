18012d702f57c8fe9f124e24218592521bdd524a
Chris Eadie <chris.eadie@outlook.com>
2020-02-03 16:31:46 +0000

    Seperated controller and view

--- app.rb
+++ app.rb
@@ -1,7 +1,7 @@
 require 'sinatra'
 
 get '/' do
-  "Hello World"
+  "Heeeeeyyyaaaaa.....heeeyyyyaaaa..."
 end
 
 get '/secret' do
@@ -9,7 +9,5 @@ get '/secret' do
 end
 
 get '/cat' do
-  "<div style='border: 3px dashed red'>
-    <img src='http://bit.ly/1eze8aE'>
-  </div>"
+  erb(:index)
 end
